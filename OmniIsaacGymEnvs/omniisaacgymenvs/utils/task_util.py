70551c03e8b163b716d8a32d2c560401cb391d4b
TakanoriHomma <takasama_1205@yahoo.co.jp>
2023-09-15 15:02:00 +0900

    fix

--- OmniIsaacGymEnvs/omniisaacgymenvs/utils/task_util.py
+++ OmniIsaacGymEnvs/omniisaacgymenvs/utils/task_util.py
@@ -46,6 +46,8 @@ def initialize_task(config, env, init_sim=True):
     from omniisaacgymenvs.tasks.crazyflie import CrazyflieTask
     from omniisaacgymenvs.tasks.go1_horizontal import Go1HorizontalTask
     from omniisaacgymenvs.tasks.go1_vertical import Go1VerticalTask
+    from omniisaacgymenvs.tasks.go1_bb import Go1BBTask
+    
     
     # Mappings from strings to environments
     task_map = {
@@ -66,6 +68,7 @@ def initialize_task(config, env, init_sim=True):
         "ShadowHandOpenAI_LSTM": ShadowHandTask,
         "Go1_Horizontal": Go1HorizontalTask,
         "Go1_Vertical": Go1VerticalTask,
+        "Go1_BB": Go1BBTask,
     }
 
     cfg = sim_config.config
--- omniisaacgymenvs/patch/README.md
+++
@@ -1,1 +0,0 @@
-copy these files into ~/.local/share/ov/pkg/isaac_sim-2022.2.1/kit/python/lib/python3.7/site-packages/rl_games/algos_torch/
